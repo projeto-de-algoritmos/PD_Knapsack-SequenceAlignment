4ec17fd3e452fa5ae94cf63874ca7f35c5aea9a1
Douglas Castro <farias.douglascastro@gmail.com>
2023-12-12 05:01:21 -0300

    Update

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,43 @@ from fastapi import (
     APIRouter, 
     Request
 )
+from datetime import datetime
+
+from .weightedIntervalScheduling import WeightedIntervalScheduling
 
 class Message(BaseModel):
     message: str
 
 
 server_router = APIRouter()
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory='templates')  
 
 @server_router.get('/')
 def route(request: Request, response_classe=HTMLResponse):
     return templates.TemplateResponse(
         'index.html', {'request': request}
     )
+
+@server_router.get('/runscheduling')
+def route_runscheduling(request: Request, response_model=Message):
+    wis = WeightedIntervalScheduling()
+    dados = request.query_params.get('tableData')
+    substrings = dados.split(';')
+    tasks = [sub.split(',') for sub in substrings]
+    tasks.insert(0, [0, 0, 0])
+    for item in tasks[1:]:
+        item[0] = datetime.fromisoformat(item[0])
+        item[1] = datetime.fromisoformat(item[1])
+        item[2] = int(item[2])
+    
+    n = len(tasks)
+    sorted_tasks = wis.sort_by_finish_time(tasks)
+    sorted_tasks.insert(0, [0, 0, 0]) 
+
+    wis.largest_compatible_indices(sorted_tasks, n-1)
+    wis.iterative_compute_opt(sorted_tasks, n-1)
+    wis.find_solution(n-1, sorted_tasks)
+
+    t_weight = sum(item[2] for item in wis.scheduled_tasks)
+
+    return {'scheduled_tasks': wis.scheduled_tasks, 't_weight': t_weight}
--- static/script.py
+++ static/script.py
@@ -1,6 +1,62 @@
+import json 
 from browser import (
-    prompt, 
-    window, 
+    ajax,
+    bind,
     document, 
     html
 )
+
+
+def addTask(evt):
+    table = document.getElementById("task-table-body")
+    newRow = document.createElement("tr")
+
+    start = document.getElementById("start").value
+    finish = document.getElementById("finish").value
+    weight = document.getElementById("weight").value
+    
+    cell1 = document.createElement("th")
+    cell2 = document.createElement("td")
+    cell3 = document.createElement("td")
+    cell4 = document.createElement("td")
+
+    cell1.text = str(len(table.children))
+    cell2.text = start
+    cell3.text = finish
+    cell4.text = weight
+
+    newRow.appendChild(cell1)
+    newRow.appendChild(cell2)
+    newRow.appendChild(cell3)
+    newRow.appendChild(cell4)
+
+    table.appendChild(newRow)
+
+def on_complete(req):
+    data = json.loads(req.text)
+    print(data.get('scheduled_tasks'))
+    document['tasks'] <= html.P(data.get('scheduled_tasks'))
+    document['t_weight'] <= html.SPAN(data.get('t_weight'))
+
+def runScheduling(evt):
+    table = document.getElementById('task-table')
+    rows = table.getElementsByTagName('tr')
+
+    data = []
+
+    for i in range(1, len(rows)):
+        cells = rows[i].getElementsByTagName('td')
+        rowData = []
+        for cell in cells:
+            rowData.append(cell.text)
+        data.append(rowData)
+
+    table_data_str = ';'.join(','.join(row) for row in data)
+
+    ajax.get(
+        'runscheduling?tableData=' + table_data_str,
+        oncomplete=on_complete,
+    )   
+
+document["runScheduling"].bind("click", runScheduling)
+document["addBtn"].bind("click", addTask)
